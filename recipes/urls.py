5c5193a9f6e6de2ac5b41233003e683137577d13
Greger1ck <val1ant2345mv@gmail.com>
2025-10-10 19:25:14 +0700

    0.2.3a

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.conf.urls.static import static
 
 from cooking import settings
-from recipes.views import IndexPage, RecipeDetailPage
+from recipes.views import IndexPage, RecipeDetailPage, SearchPage, CategoryList
 
 urlpatterns = [
     path('', IndexPage.as_view(), name='home'),
     path('recipe/<slug:slug>/', RecipeDetailPage.as_view(), name='recipe'),
+    path('search', SearchPage.as_view(), name='search'),
+    path('category/<slug:slug>/', CategoryList.as_view(), name='category'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,6 @@
+from os import name
 from django.shortcuts import render
+from django.template import context
 from django.views.generic import DetailView, ListView
 
 from recipes.models import Category, Recipe
@@ -18,3 +20,28 @@ class RecipeDetailPage(DetailView):
     model = Recipe
     context_object_name = "recipe"
     template_name = "recipes/recipe_detail.html"
+
+
+class SearchPage(ListView):
+    template_name = "recipes/search.html"
+    context_object_name = "recipes"
+    model = Recipe
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.GET.get('search', None)
+        if search :
+            queryset = queryset.filter(name__icontains=search)
+        return queryset
+    
+class CategoryList(DetailView):
+    template_name = 'recipes/search.html'
+    context_object_name = 'category'
+    model = Category
+
+    def get_context_data(self, **kwargs):
+            obj = self.get_object()
+            context = super().get_context_data(**kwargs)
+            context["recipes"] = obj.recipes_by_cat.all()
+            return context
+        
